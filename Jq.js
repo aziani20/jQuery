a84a8768e8c60a77d0cab88265e4d836a7c5c3a9
aziani00 <applehijau19.ptss@gmail.com>
2020-09-25 11:28:02 +0800

    kedua kali upload

--- Jq.js
+++ Jq.js
@@ -1,5 +1,13 @@
 $(function () {
-    $('#buttonjq').click(function () {
+    $('#btnNAMA').click(function () {
         $('#html').slideToggle('slow');
+    });
+
+    $('#btnALAMAT').click(function () {
+        $('#css').slideToggle('slow');
+    });
+
+    $('#btnJABATAN').click(function () {
+        $('#js').slideToggle('slow');
     });
 });
